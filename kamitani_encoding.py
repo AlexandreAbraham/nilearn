208acab3a82545e1d2f84e6445ee548fb03468d1
Michael <michael.eickenberg@gmail.com>
2013-07-29 13:56:18 +0200

    very simple encoding

--- kamitani_encoding.py
+++ kamitani_encoding.py
@@ -55,14 +55,14 @@ n_features = X_train.shape[1]
 
 
 ### very simple encoding using ridge regression
-from sklearn.linear_model import RidgeCV
+from sklearn.linear_model import RidgeCV, Ridge
 
 # ridge = RidgeCV(alphas=np.logspace(1, 3, 10), cv=4)
 
 from sklearn.cross_validation import cross_val_score
 from sklearn.externals.joblib import Parallel, delayed
 
-alphas = np.logspace(1, 3, 5)
+# alphas = np.logspace(1, 3, 5)
 
 # scores = Parallel(n_jobs=20)(delayed(cross_val_score)(RidgeCV(alphas, cv=4),
 #     y_train.reshape(len(y_train), -1), target, cv=5, n_jobs=5, verbose=100)
@@ -70,34 +70,54 @@ alphas = np.logspace(1, 3, 5)
 
 
 # preselect voxels using f_select
-from sklearn.feature_selection import f_classif
+# from sklearn.feature_selection import f_classif
 
-f_scores = np.array([f_classif(X_train, y_train.reshape(-1, 100)[:, i])[0]
-                     for i in range(100)])
+# f_scores = np.array([f_classif(X_train, y_train.reshape(-1, 100)[:, i])[0]
+#                      for i in range(100)])
 
-n_voxels = 1000
+# n_voxels = 1000
 
-voxel_indices = f_scores.max(0).argsort()[::-1][:n_voxels]
+# voxel_indices = f_scores.max(0).argsort()[::-1][:n_voxels]
 
-scores = Parallel(n_jobs=20)(delayed(cross_val_score)(RidgeCV(alphas, cv=4),
-    y_train.reshape(len(y_train), -1), target, cv=5, n_jobs=1, verbose=100)
-    for target in X_train.T[voxel_indices])
+estimator = Ridge(alpha=100.)
+from sklearn.cross_validation import KFold
+import time
+t0 = time.time()
+print "doing ridge regression"
+cv = KFold(len(y_train), 10)
+predictions = [Ridge(alpha=100.).fit(y_train.reshape(-1, 100)[train],
+                                     X_train[train]).predict(
+                                         y_train.reshape(-1, 100)[test])
+               for train, test in cv]
 
-scores = np.array(scores).T
+print "scoring"
+scores = [1. - (((X_train[test] - pred) ** 2).sum(axis=0) /
+           ((X_train[test] - X_train[test].mean(axis=0)) ** 2).sum(axis=0))
+for pred, (train, test) in zip(predictions, cv)]
 
-n_voxels_for_lasso = 200
-indices_for_lasso = scores.mean(0).argsort()[::-1][:n_voxels_for_lasso]
-from sklearn.linear_model import LassoLarsCV
+mean_scores = np.array(scores).mean(axis=0)
 
-lasso = LassoLarsCV(max_iter=100,)
+t1 = time.time()
+print "Done in %1.2fs" % (t1 - t0)
+# scores = Parallel(n_jobs=20)(delayed(cross_val_score)(RidgeCV(alphas, cv=4),
+#     y_train.reshape(len(y_train), -1), target, cv=5, n_jobs=1, verbose=100)
+#     for target in X_train.T[voxel_indices])
 
-receptive_fields = []
+# scores = np.array(scores).T
 
-y_train_centered = y_train.reshape(-1, 100)
-y_train_centered = (y_train_centered -
-                    y_train_centered.mean(0)) / y_train_centered.std(0)
+# n_voxels_for_lasso = 200
+# indices_for_lasso = scores.mean(0).argsort()[::-1][:n_voxels_for_lasso]
+# from sklearn.linear_model import LassoLarsCV
 
-for i, index in enumerate(indices_for_lasso):
-    print "%d %d" % (i, index)
-    receptive_fields.append(
-        lasso.fit(y_train.reshape(-1, 100), X_train[:, index]).coef_)
+# lasso = LassoLarsCV(max_iter=100,)
+
+# receptive_fields = []
+
+# y_train_centered = y_train.reshape(-1, 100)
+# y_train_centered = (y_train_centered -
+#                     y_train_centered.mean(0)) / y_train_centered.std(0)
+
+# for i, index in enumerate(indices_for_lasso):
+#     print "%d %d" % (i, index)
+#     receptive_fields.append(
+#         lasso.fit(y_train.reshape(-1, 100), X_train[:, index]).coef_)
